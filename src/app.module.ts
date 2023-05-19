35050af998faf51b4f88b7fceb9aa61ae577acbe
Camilo Figueroa <cigotete@gmail.com>
2023-05-19 13:30:25 -0500

    Added Mongo connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-books'),
     BookModule,
   ],
 })
