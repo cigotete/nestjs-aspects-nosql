08bc503dc1499e1d74d75df9d77ab9f0a1f89c0e
Camilo Figueroa <cigotete@gmail.com>
2023-05-20 11:58:28 -0500

    Added some props to schemas.

--- src/book/entities/author.entity.ts
+++ src/book/entities/author.entity.ts
@@ -1,6 +1,11 @@
+import { Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export interface Author extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
   name: string;
   birth_year: number;
   death_year: number;
--- src/book/entities/book.entity.ts
+++ src/book/entities/book.entity.ts
@@ -1,9 +1,21 @@
+import { Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { Author } from './author.entity';
 import { Format } from './format.entity';
 export class Book extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
   idremote: number;
+  @Prop({
+    required: true,
+    unique: true,
+  })
   title: string;
+  @Prop({
+    required: true,
+  })
   authors: Author[];
   translators: string[];
   subjects: string[];
